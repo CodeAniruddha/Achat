fca3d2d8ca961953825fb5b98f5d792eb4e561a2
CodeAniruddha <106012719+CodeAniruddha@users.noreply.github.com>
2022-06-18 16:15:31 +0530

    Add files via upload

---
+++ utils/messages.js
@@ -0,0 +1,12 @@
+// Made by Aniruddha
+const moment = require("moment");
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  };
+}
+
+module.exports = formatMessage;
